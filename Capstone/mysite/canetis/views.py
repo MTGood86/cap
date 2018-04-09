053e880d5f26f94776203c9066e2ab255c6b583b
Michael Good <mtgood86@gmail.com>
2018-04-09 16:42:14 -0700

    Added columns, frqs onclick events.

--- Capstone/mysite/canetis/views.py
+++ Capstone/mysite/canetis/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
-from .models import Sequencer
-from django.http import HttpResponse
-from django.template import loader
 
 
 def index(request):
-    return render(request, 'canetis/index.html')
+    return render(request, 'canetis/index.html', )
 
